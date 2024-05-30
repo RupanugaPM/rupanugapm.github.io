f49b26f2fccc7ed767a2deac8767c542f022bf3a
RupanugaPM <92666237+RupanugaPM@users.noreply.github.com>
2024-05-30 10:16:50 +0530

    changed some css and javascript

---
+++ scripts.js
@@ -0,0 +1,27 @@
+<script>
+// scripts.js
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// navigation bar
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
