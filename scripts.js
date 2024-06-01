8990b6d410b10deb52a785568fa2fbe8e51ca574
RupanugaPM <92666237+RupanugaPM@users.noreply.github.com>
2024-06-01 13:15:49 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-<script>
+
 // scripts.js
 
 document.querySelectorAll('nav ul li a').forEach(anchor => {
@@ -24,4 +24,22 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-</script>
+
+
+// tabs function to open cp site
+function opensite(evt, siteName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(siteName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+
+// Get the element with id="defaultOpen" and click on it
